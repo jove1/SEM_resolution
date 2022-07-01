46ce9ca97649a08375c14d8ee2ae0dcdf380a1df
Jozef Vesely <vesely@gjh.sk>
2022-07-01 10:42:47 +0200

    Add README and requirements

---
+++ README.md
@@ -0,0 +1,41 @@
+# SEM Resolution
+
+This program is designed to determine SEM resolution form the image of gold nanoparticles on carbon.
+The gaussian profile is fitted to the particle edges, and the width (&sigma;) is repoted.
+
+
+## Instal
+
+Install [Python 3](https://www.python.org/downloads/) according your operating system. 
+Then the required libraries:
+
+
+```
+pip3 install -r requirements.txt 
+```
+
+## Usage
+
+```
+./resolution.py --help
+usage: resolution.py [-h] [--crop TOP BOTTOM LEFT RIGHT] [--pixel PIXEL] [--wait] [--interval INTERVAL] [--length LENGTH] [--fraction FRACTION] [FILE [FILE ...]]
+
+Determine SEM resolution.
+
+positional arguments:
+  FILE                  *.tif files to process, generates syntetic data if no files are specified
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --crop TOP BOTTOM LEFT RIGHT
+                        crop image margins (default: 0 0 0 0)
+  --pixel PIXEL         pixel size (default: 1nm)
+  --wait                wait after each image
+  --interval INTERVAL   profile interval along contour (default: 10)
+  --length LENGTH       profile length (default: 75)
+  --fraction FRACTION   fraction of included profiles ordered by fit residual (default: 0.2)
+```
+
+## Output
+
+![](output.png)
--- resolution.py
+++ resolution.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import scipy.ndimage
+import scipy
 import matplotlib.pyplot as plt
 import PIL.Image
 import skimage
 
-def make_particles(N=2048, M=200,
+def make_particles(img_size=2048, num_particles=200,
     sep=5, size=100, size_sigma=0.5, value_sigma=0.2):
 
-    data = np.zeros((N,N))
+    data = np.zeros((img_size,img_size))
     circles = []
-    while len(circles) < M:
-        x, y = np.random.uniform(0, N, 2)
+    while len(circles) < num_particles:
+        x, y = np.random.uniform(0, img_size, 2)
         r = np.random.lognormal(np.log(size), size_sigma)
         v = np.random.lognormal(0, value_sigma)
         for x1, y1, r1 in circles:
@@ -23,42 +23,22 @@ def make_particles(N=2048, M=200,
             data[coords] = v
 
             circles.append((x,y,r))
-            print(".", end="", flush=True)
+            print(end="\rgenerating synthetic image {:.1f}%".format(100*len(circles)/num_particles), flush=True)
     print()
     return data
 
-def kittler_illingworth(h):
-    "Kittler, J. & Illingworth, J. Minimum error thresholding. Pattern Recognit. 19, 41–47 (1986)."
-    i = np.arange(h.size)
 
-    h1 = np.cumsum(h)
-    ih1 = np.cumsum(i*h)
-    iih1 = np.cumsum(i*i*h)
-
-    # this has precision problems with floats, 
-    # we have int histograms though
-
-    h2 = h1[-1]-h1 
-    ih2 = ih1[-1]-ih1
-    iih2 = iih1[-1]-iih1
-    
-    with np.errstate(invalid="ignore", divide="ignore"):
-        s1 = iih1/h1 - ih1*ih1/h1/h1
-        s2 = iih2/h2 - ih2*ih2/h2/h2
-
-        criterion = np.where((h1==0)|(s1==0), 0, h1*np.log(s1/h1/h1)) + \
-                    np.where((h2==0)|(s2==0), 0, h2*np.log(s2/h2/h2))
-
-    return np.argmin(criterion), criterion
-
-def resolution(data, label):
+def resolution(data, label, options):
     print(label)
 
     #
     # Crop databar
     #
 
-    data = data[:2048]
+    data = data[
+            options.crop[0]:data.shape[0]-options.crop[1],
+            options.crop[2]:data.shape[1]-options.crop[3],
+            ]
 
     #
     # Check image size and bitdepth
@@ -75,13 +55,17 @@ def resolution(data, label):
 
     data = data/65535
 
-    import matplotlib.pyplot as plt
-    fig, ax = plt.subplots(2,2)
-    ax = ax.ravel()
+    fig = plt.figure(figsize=(10, 7.5))
+    ax_image = fig.add_subplot(231)
+    ax_zoom = fig.add_subplot(232)
+    ax_hist = fig.add_subplot(233)
+    ax_fit = fig.add_subplot(234)
+    ax_polar = fig.add_subplot(235, polar=True)
+    ax_text = fig.add_subplot(236)
 
-    ax[0].set_title(label)
-    ax[0].imshow(data, cmap="gray", vmin=0, vmax=1)
-    ax[1].imshow(data[:512,:512], cmap="gray", vmin=0, vmax=1)
+    fig.suptitle(label)
+    ax_image.imshow(data, cmap="gray", vmin=0, vmax=1)
+    ax_zoom.imshow(data[:512,:512], cmap="gray", vmin=0, vmax=1)
 
     #
     # Calculate histogram and check over/under exposure
@@ -94,12 +78,13 @@ def resolution(data, label):
 
     bpos = (bins[1:] + bins[:-1])/2
     bwidth = bins[:-1]-bins[1:]
-    ax[2].bar(bpos[:8], h[:8], bwidth[:8], color="r" if too_dark else "g")
-    ax[2].bar(bpos[8:-8], h[8:-8], bwidth[8:-8], color="C0")
-    ax[2].bar(bpos[-8:], h[-8:], bwidth[-8:], color="r" if too_bright else "g")
-    ax[2].axvline(bins[8], ls="--", c="k")
-    ax[2].axvline(bins[-9], ls="--", c="k")
-    ax[2].set_xlim(0,1)
+    ax_hist.bar(bpos[:8], h[:8], bwidth[:8], color="r" if too_dark else "g")
+    ax_hist.bar(bpos[8:-8], h[8:-8], bwidth[8:-8], color="C0")
+    ax_hist.bar(bpos[-8:], h[-8:], bwidth[-8:], color="r" if too_bright else "g")
+    ax_hist.axvline(bins[8], ls="--", c="k")
+    ax_hist.axvline(bins[-9], ls="--", c="k")
+    ax_hist.set_xlim(0,1)
+    ax_hist.set_yticks([])
 
     if too_dark:
         print("too many dark pixels")
@@ -111,55 +96,163 @@ def resolution(data, label):
         return
   
     #
-    # Find optimal threshold for gaussian mixture
+    # Find optimal threshold
     #
 
-    thresh_index, criterion = kittler_illingworth(h) 
-    thresh = bins[thresh_index+1]
-    ax[2].twinx().plot(bins[1:], criterion, "C1")
-
-    bad_thresh = h[:thresh_index+1].sum() < 0.25*h.sum() or \
-                 h[thresh_index+1:].sum() < 0.25*h.sum()
-
-    ax[2].axvline(thresh, ls="--", c="r" if bad_thresh else "g")
-
-    if bad_thresh:
+    try:
+        thresh_index = skimage.filters.threshold_minimum(hist=h)
+    except RuntimeError:
         print("bimodal distribution not found. Too much noise?")
         return
+
+    if h[:thresh_index].sum() < 0.1*h.sum():
+        print("too small background area")
+        return
+
+    if h[thresh_index:].sum() < 0.1*h.sum():
+        print("too small particle area")
+        return
+
+    thresh = bins[thresh_index]
+    ax_hist.axvline(thresh, ls="--", c="C1")
 
     #
     # Normals to blob contours
     #
 
-    N = 10
-    L = 30
+    N = options.interval
+    L = options.length
     contours = skimage.measure.find_contours(scipy.ndimage.gaussian_filter(data, 3).T, thresh)
     profiles = []
+    normals = []
     for c in contours:
         for ca, cb in zip(c[::N][:-1], c[::N][1:]):
             p = (ca+cb)/2 # center
             q = cb-ca # tangent 
             q = np.array([-q[1], q[0]]) # normal
             q /= np.hypot(q[0], q[1]) # normalize
-            pa, pb = p+L*q,  p-L*q # endpoints
-
+            pa, pb = p+L/2*q,  p-L/2*q # endpoints
+            normals.append(q)
             profiles.append( (pa, pb) )
+    normals = np.array(normals)
+    profiles = np.array(profiles)
+
+    #
+    # Fit profile shape
+    #
+
+    from scipy.special import ndtr as stepf
+    _sqrt2pi = np.sqrt(2*np.pi)
+    dstepf = lambda x: np.exp(-x*x/2)/_sqrt2pi
+
+    def f(par, x, y):
+        a, b, m, s = par
+        return a + (b-a) * stepf( (x-m)/s ) - y
+
+    def df(par, x, y):
+        a, b, m, s = par
+        arg = (x-m)/s
+        ret = np.empty((4, x.size))
+        stepf(arg, out=ret[1])
+        ret[0] = 1-ret[1]
+        ret[2] = (a-b)/s*dstepf(arg)
+        ret[3] = ret[2]*arg
+        return ret
+
+    def profile_line(data, a, b, N):
+        x = np.linspace(a[0], b[0], N, dtype=int).clip(0, data.shape[1]-1)
+        y = np.linspace(a[1], b[1], N, dtype=int).clip(0, data.shape[0]-1)
+        return data[y, x]
+
+    results = []
+    residuals = []
+    x = np.arange(L)
+    
+    num = len(profiles)
+    print("{} candicate profiles".format(num))
+    for i, prof in enumerate(profiles,1):
+        # this is nice but very slow
+        #y = skimage.measure.profile_line(data.T, prof[0], prof[1], order=0, mode="nearest")
+        y = profile_line(data, prof[0], prof[1], L)
+
+        par0 = [ y[:5].mean(), y[-5:].mean(), L/2, 5]
+        res = scipy.optimize.leastsq(f, x0=par0, args=(x, y), Dfun=df, col_deriv=True, full_output=True)
+        par = res[0]
+
+        results.append(par)
+        residuals.append( np.mean(f(par, x, y)**2) )
+        print(end="\rfitting {:.1f}%".format(100*i/num), flush=True)
+    print()
+    results = np.array(results)
+    residuals = np.array(residuals)
+    
+    #
+    # Present results
+    #
+    m = results[:,2]
+    s = np.abs( results[:,3] )
+    t = np.arctan2(normals[:,1], normals[:,0])
+    
+    # exclude bogus fits
+    residuals[m>L] = np.inf
+    residuals[m<0] = np.inf
+
+    idx = np.argsort(residuals)[:int(len(residuals)*options.fraction)]
+
+    for i in idx:
+        y = profile_line(data, profiles[i][0], profiles[i][1], L)
+        ax_fit.plot(x, y, c="C0", lw=0.2)
+        ax_fit.plot(x, f(results[i], x, 0), c="C1", lw=0.2)
+    ax_fit.grid(True)
 
     from matplotlib.collections import LineCollection
+    c_profiles = LineCollection(profiles[idx], colors="C1")
     c_contours = LineCollection(contours, colors="C0")
-    c_profiles = LineCollection(profiles, colors="C1")
-    ax[0].add_collection(c_profiles)
-    ax[0].add_collection(c_contours)
+
+    ax_image.add_collection(c_profiles)
+    ax_image.add_collection(c_contours)
+
+    t = t[idx]
+    s = s[idx]
+
+    p25, p50, p75 = np.percentile(s, [25,50,75])
+    report = (
+        "#      {}\n"
+        "mean   {:.2f} px\n"
+        "std    {:.2f} px\n"
+        "25%    {:.2f} px\n"
+        "median {:.2f} px\n"
+        "75%    {:.2f} px\n"
+        ).format(
+            len(idx),
+            np.mean(s), 
+            np.std(s), 
+            p25,
+            p50, # np.median(s)
+            p75
+        )
+    print(report, end="")
+
+    ax_polar.plot(t, s, ".", zorder=0, ms=3)
+    ax_polar.set_rlim(0, 2*np.mean(s))
+    ax_text.axis("off")
+    ax_text.text(0.5, 0.5, report, font="monospace", ha="center", multialignment="left", va="center", transform=ax_text.transAxes)
+
+    fig.tight_layout()
 
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description='Determine SEM resolution.')
-    parser.add_argument('files', nargs="*", metavar="FILE", help="*.tif files to process")
+    parser.add_argument('files', nargs="*", metavar="FILE", help="*.tif files to process, generates syntetic data if no files are specified")
     parser.add_argument("--crop", nargs=4, type=int, metavar=("TOP", "BOTTOM", "LEFT", "RIGHT"),  default=(0, 0, 0, 0),
-            help="crop image margins (default: 0, 0, 0, 0)")
+            help="crop image margins (default: 0 0 0 0)")
     parser.add_argument("--pixel", type=float, default=1, help="pixel size (default: 1nm)")
     parser.add_argument("--wait", action="store_true", help="wait after each image")
+    parser.add_argument("--interval", type=int, default=10, help="profile interval along contour (default: 10)")
+    parser.add_argument("--length", type=int, default=75, help="profile length (default: 75)")
+    parser.add_argument("--fraction", type=float, default=0.2, help="fraction of included profiles ordered by fit residual (default: 0.2)")
+
     args = parser.parse_args()
 
     if not args.files:    
@@ -178,7 +271,7 @@ if __name__ == "__main__":
 
             data = (data.clip(0, 1) * 65535).astype("uint16")
 
-            resolution(data, "sigma={}".format(sigma))
+            resolution(data, "sigma={}".format(sigma), args)
             print()
             if args.wait:
                 plt.show()
@@ -190,7 +283,7 @@ if __name__ == "__main__":
         for fname in args.files:
             img = PIL.Image.open(fname)
 
-            resolution(np.asarray(img), fname)
+            resolution(np.asarray(img), fname, args)
             print()
             if args.wait:
                 plt.show()
